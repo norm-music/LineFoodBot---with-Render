fe65da1e3e70f84aa3f3dabf9a2b470a8a834a3d
Norman Yu <pdaxing832@gmail.com>
2026-05-28 02:27:25 +0800

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
+import json
 from flask import Flask, request, abort
-
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
@@ -25,7 +25,10 @@ from linebot.v3.messaging import (
     MessageAction,
     DatetimePickerAction,
     QuickReply,
-    QuickReplyItem
+    QuickReplyItem,
+    FlexMessage,
+    FlexContainer,
+
 )
 
 from linebot.v3.webhooks import (
@@ -44,6 +47,7 @@ CHANNEL_SECRET = os.environ.get('LINE_CHANNEL_SECRET')
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # 檢查 LINE 的數位簽章，確保請求真的來自 LINE 伺服器
@@ -61,7 +65,15 @@ def callback():
 # 當收到文字訊息時的處理邏輯
 @handler.add(FollowEvent)
 def handle_follow(event):
-    print(f'Got {event.type}event')
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+    
+        line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    replyToken=event.reply_token,
+                    messages=["已加入好友,輸入「很餓」來使用機器人 !"]
+                )
+            )
 
 
 @handler.add(MessageEvent, message=TextMessageContent)
@@ -71,30 +83,77 @@ def message_text(event):
         line_bot_api = MessagingApi(api_client)
         
         if text == "很餓":
-            url="https://linefoodbot.onrender.com/static/Denia.jpg"
-            app.logger.info("url="+url)
-            button_template = ButtonsTemplate(
-                thumbnail_image_url=url,
-                title='您的餐廳小幫手',
-                text='解決您的選擇障礙',
-                actions=[
-                    MessageAction(label="早安", text="早安"),
-                    DatetimePickerAction(label='選擇時間', data='時間', mode='datetime'),
-                    LocationAction(label="回傳我的位置")
-                ])
-            template_message= TemplateMessage(
-                alt_text="這是選擇機器人",
-                template=button_template
-            )
-
-            line_bot_api.reply_message(
+            line_flex_json={
+                {
+                    "type": "bubble",
+                    "hero": {
+                    "type": "image",
+    "size": "full",
+    "aspectRatio": "4:3",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "https://line.me/"
+    },
+    "url": "https://linefoodbot.onrender.com/static/Denia.jpg"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "您的餐廳小幫手",
+        "weight": "bold",
+        "size": "xl"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "解決選擇困難, 刻不容緩 !",
+            "size": "sm"
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "location",
+          "label": "定位",
+        },
+        "margin": "md",
+        "style": "primary"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "編輯表單",
+          "uri": "http://linecorp.com/"
+        },
+        "margin": "md"
+      }
+    ]
+  }
+}
+            }
+        line_flex_str=json.dump(line_flex_json)
+        line_bot_api.reply_message(
                 ReplyMessageRequest(
-                    replyToken=event.reply_token,
-                    messages=[template_message]
+                    reply_token=event.reply_token,
+                    messages=[FlexMessage(alt_text='詳細說明',contents=FlexContainer.from_json(line_flex_str))]
                 )
-            )
+        )
 
-
-
+        
 if  __name__=="__main__":
     app.run()
