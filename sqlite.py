a2f344124f2b6fab8dae89617ad51babc6570966
Norman Yu <pdaxing832@gmail.com>
2026-05-30 16:07:24 +0800

    update database

--- sqlite.py
+++ sqlite.py
@@ -24,22 +24,8 @@ def init_db():
             search_radius REAL DEFAULT 5.0
         )
     ''')
-
-    cursor.execute('SELECT COUNT(*) FROM restaurants')
-    if cursor.fetchone()[0] == 0:
-        test_data = [
-            ('美味拉麵屋', '台北市信義區XX路', '日式料理', '$$', 25.033964, 121.564468, 1),
-            ('超讚義大利麵', '台北市大安區YY街', '義式料理', '$$$', 25.042123, 121.543210, 1),
-            ('巷口乾麵', '台北市信義區ZZ巷', '小吃', '$', 25.035000, 121.567000, 0)
-        ]
-        cursor.executemany('''
-            INSERT INTO restaurants (name, address, category, price_range, latitude, longitude, is_favorite)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', test_data)
-
+    
     conn.commit()
     conn.close()
 
 
-if __name__ == "__main__":
-    init_db()
